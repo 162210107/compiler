dcad878e5560fae4674c603d99db1780870e99bc
蔡蕾 <2256589622@qq.com>
2024-12-12 15:59:29 +0800

    文档更新完成，解释器初步完成

---
+++ Include/Interpreter.hpp
@@ -0,0 +1,36 @@
+#ifndef _INTER_H
+#define _INTER_H
+#include <PCode.hpp>
+#include <types.hpp>
+using namespace std;
+
+#define RA 0
+#define DL 1
+#define GLO_DIS 2
+#define DISPLAY 3
+// P代码的解释器
+class Interpreter {
+public:
+    size_t pc; // 指令寄存器
+    size_t top; // 活动记录栈顶，并非实际开辟的空间栈顶
+    size_t sp; // 当前活动记录基地址
+    vector<int> running_stack; // 数据运行栈    
+    void run();
+    void clear();
+    void Init();
+
+private:
+    void lit(Operation op, int L, int a);
+    void opr(Operation op, int L, int a);
+    void lod(Operation op, int L, int a);
+    void sto(Operation op, int L, int a);
+    void cal(Operation op, int L, int a);
+    void alc(Operation op, int L, int a);
+    void jmp(Operation op, int L, int a);
+    void jpc(Operation op, int L, int a);
+    void red(Operation op, int L, int a);
+    void wrt(Operation op, int L, int a);
+};
+
+extern Interpreter interpreter;
+#endif
